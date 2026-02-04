d7edc362445d5391cc2db10b92cc38164f7c98ec
212445151AL <212445151@qq.com>
2026-02-04 15:24:04 +0800

    Update em.h

--- src/em.h
+++ src/em.h
@@ -1,13 +1,11 @@
 // Simple namespace and EM tools for EM modeling
 
-// Author:     Hongbo Yao
-// Institute:  School of Geosciences and Info-Physics,
-//             Central South University (CSU)
-// Email:      yaohongbo@csu.edu.cn
-// Date:       2021/01/17
+// Author:     Liangyu Xie
+// Institute:  Central South University (CSU)            
+// Email:      8211221219@csu.edu.cn
+// Date:       2026/02/04
 
-// GitHub Page: https://github.com/hongbo-yao
-// Researchgate Page: https://www.researchgate.net/profile/Hongbo_Yao2
+// GitHub Page: https://github.com/212445151AL
 
 //  ************************** Changelog **************************
 // 2021/07/22
